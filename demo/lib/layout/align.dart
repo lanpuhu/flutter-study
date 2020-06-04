b3b42d9b402dcf2db1cf809e7fef81f5a834c7b1
lanpuhu <lanpuhu@126.com>
2020-06-04 10:45:49 +0800

    Update layout align widget basic info
    Signed-off-by: lanpuhu <lanpuhu@126.com>

--- demo/lib/layout/align.dart
+++ demo/lib/layout/align.dart
@@ -5,14 +5,43 @@ class LayoutAlignPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Page'),),
-      body: Center(
-        child: RaisedButton(
-          child: Text("Go back"),
-          onPressed: () {
-            Navigator.pop(context);
-          }
-        ),
+        title: Text('Align Widget'),),
+      body: Row(
+        children: <Widget>[
+          Container(
+            width: 120,
+            height: 120,
+            color: Colors.blue[50],
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: FlutterLogo(
+                size: 60,
+              ),
+            ),
+          ),
+          Container(
+            width: 120,
+            height: 120,
+            color: Colors.blue[100],
+            child: Align(
+              alignment: Alignment.center,
+              child: FlutterLogo(
+                size: 60,
+              ),
+            ),
+          ),
+          Container(
+            width: 120,
+            height: 120,
+            color: Colors.blue[50],
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: FlutterLogo(
+                size: 60,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
